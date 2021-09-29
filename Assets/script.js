e3de962d62342fd3daf519865b964c861239a3b8
unknown <mccullochryan636@gmail.com>
2021-09-29 17:22:26 -0600

    added local storage funtions and moment function

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,36 @@
+$().ready(function todaysDate(){
+    let currentDay = $("#currentDay");
+    currentDay.text(moment().format("MMM Do, YYYY"));
+});
+
+
+
+
+
+
+
 let saveBtns = $("button")
 
 saveBtns.on('click', function(event){
 // console.log(event.target.previousSibling.previousSibling);
-   // document.getElementById("Nine")   
+
         var id = $(this).siblings("input").attr("id");
         var value = $(this).siblings("input").val();
        localStorage.setItem(id, value);
        return console.log(id, value)
 
     });   
+
+ 
+$("form-control").ready(function(event){
+        // pulling events from local storage
+        
+        var id = $(this).siblings("input").attr("id");
+        var savedEvent = localStorage.getItem(id)
+        $(this).val(savedEvent);
+        
+    });   
+        
+
+
+
